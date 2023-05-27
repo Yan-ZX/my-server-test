1b4c4646a7eb5aaf137e0bca3029b9295b010995
yanzx-c <yanzx-c@glodon.com>
2023-05-27 22:38:54 +0800

    自定义serviceLog

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger('APP_SERVICE_LOG');
   getHello(): string {
     return 'Hello World!';
   }
 
   save(params: string): string {
+    this.logger.log('调用save方法');
     return `sava ! ${params}`;
   }
 }
